4e0f7d3a54ec31aa02b5bff5b96e469680ae8215
Erik Paasonen <17558034+erikpaasonen@users.noreply.github.com>
2019-04-01 13:07:06 -0600

    Start of pipeline resource

--- azurerm/provider.go
+++ azurerm/provider.go
@@ -231,6 +231,7 @@ func Provider() terraform.ResourceProvider {
 			"azurerm_data_factory_pipeline":                  resourceArmDataFactoryPipeline(),
 			"azurerm_data_factory_sql_server_table_dataset":  resourceArmDataFactorySQLServerTableDataset(),
 			"azurerm_data_factory_dataset_sql_server_table":  resourceArmDataFactoryDatasetSQLServerTable(),
+			"azurerm_data_factory_pipeline":                  resourceArmDataFactoryPipeline(),
 			"azurerm_data_lake_analytics_account":            resourceArmDataLakeAnalyticsAccount(),
 			"azurerm_data_lake_analytics_firewall_rule":      resourceArmDataLakeAnalyticsFirewallRule(),
 			"azurerm_data_lake_store_file":                   resourceArmDataLakeStoreFile(),
